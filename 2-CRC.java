00b67f6b2e116649675cd12bd00f272db8e09890
Pruthvi K P <117765336+pruthvikp@users.noreply.github.com>
2024-01-09 19:41:15 +0530

    Update 2-CRC.java

--- 2-CRC.java
+++ 2-CRC.java
@@ -1,48 +1,66 @@
 // Write a program for error detecting code using CRC-CCITT (16- bits).
-
 import java.util.*;
 
-public class CRC {
-    String calCRC(String data, String poly, boolean errCheck) {
-        StringBuilder rem = new StringBuilder(data);
-        if (!errCheck) {
-            for (int i = 0; i < poly.length() - 1; i++)
-                rem.append("0");
-        }
-        for (int i = 0; i < rem.length() - poly.length() + 1; i++) {
-            if (rem.charAt(i) == '1') {
-                for (int j = 0; j <= poly.length() - 1; j++)
-                    rem.setCharAt(i + j, rem.charAt(i + j) == poly.charAt(j) ? '0' : '1');
-            }
-        }
-        return rem.substring(rem.length() - poly.length() + 1);
-    }
+public class CRC{
+	
+	// This method performs polynomial division using XOR operation.
+	public static String calCRC(String data, String poly, boolean errcheck){
+		StringBuilder rem=new StringBuilder(data);
+		if(!errcheck){
+			for(int i=0;i<poly.length()-1;i++)
+				rem.append("0");
+		}
+		for(int i=0;i<rem.length()-poly.length()+1;i++){
+			if(rem.charAt(i)=='1'){
+				for(int j=0;j<poly.length();j++){
+					rem.setCharAt( i+j, rem.charAt(i+j)==poly.charAt(j)? '0':'1' );
+				}
+			}
+		}
+		return rem.substring(rem.length()-poly.length()+1);
+	}	
 
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		CRC obj=new CRC();
+		
+		String poly="10000100010001010";
+		
+		System.out.println("Enter the data");
+		String data=sc.nextLine();
+		String rem=obj.calCRC(data,poly,false);
+		String codeword=data+rem;
+		System.out.println("Codeword: "+codeword);
+		
+		System.out.println("Enter the received word");
+		String recdata=sc.nextLine();
+		String recrem=obj.calCRC(recdata,poly,true);
+		if(Integer.parseInt(recrem,2)==0)
+			System.out.println("No error");
+		else
+			System.out.println("There is an error in the received codeword");
+		// recRem is a binary string representing the remainder obtained after dividing the received code word by the polynomial.
+        // Integer.parseInt(recRem, 2) converts this binary string to its equivalent integer value in base 10.
+	}
 
-    public static void main(String[] args) {
-        CRC ob = new CRC();
-        Scanner sc = new Scanner(System.in);
-
-        String poly = "10000100010001010";
-
-        System.out.print("Enter the Data : ");
-        String data = sc.nextLine();
-
-        String rem = ob.calCRC(data, poly, false);
-        String codeWord = data + rem;
-
-        System.out.println("Code Word : " + codeWord);
-
-        System.out.print("Enter recieved Code Word : ");
-        String recData = sc.nextLine();
-
-        String recRem = ob.calCRC(recData, poly, true);
-
-        if (Integer.parseInt(recRem, 2) == 0) {
-            System.out.println("No Error");
-        }
-        else
-            System.out.println("There was an error");
-        // recRem is a binary string representing the remainder obtained after dividing the received code word by the polynomial.
-        // Integer.parseInt(recRem, 2) converts this binary string to its equivalent integer value in base 10.    }
 }
+
+
+/*
+Output:
+------------------------------------------
+Enter the data
+1234
+Codeword: 12340111111111100110
+Enter the received word
+12340111111111100110
+No error
+-------------------------------------------
+Enter the data
+1234
+Codeword: 12340111111111100110
+Enter the received word
+12340111111111100111    
+There is an error in the received codeword
+--------------------------------------------
+*/
