0d24dc589497f78c05ab6eb99dc861d1fc42c491
Pruthvi K P <117765336+pruthvikp@users.noreply.github.com>
2024-01-09 16:24:51 +0530

    Update TCPC.java

--- 4-TCP/TCPC.java
+++ 4-TCP/TCPC.java
@@ -1,29 +1,44 @@
+import java.util.*;
 import java.net.*;
 import java.io.*;
 
-public class TCPC {
-    public static void main(String[] args) throws Exception {
-        Socket sock = new Socket("127.0.0.1", 4000);
-        
-        System.out.println("Enter the filename");
-        BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-        String fname = keyRead.readLine();
-
-        OutputStream ostream = sock.getOutputStream();
-        PrintWriter pwrite = new PrintWriter(ostream, true);
-        pwrite.println(fname);
-
-        InputStream istream = sock.getInputStream();
-        BufferedReader socketRead = new BufferedReader(new InputStreamReader(istream));
-
-        String str;
-        while ((str = socketRead.readLine()) != null) {
-            System.out.println(str);
-        }
-        sock.close();
-        pwrite.close();
-        socketRead.close();
-        keyRead.close();
-    }
+public class TCPC{
+	public static void main(String[] args) throws Exception{
+		
+		Socket sock=new Socket("127.0.0.1",4000);
+		
+		System.out.println("Enter the filename");
+		Scanner sc=new Scanner(System.in);
+		String fname=sc.nextLine();
+		
+		OutputStream ostream=sock.getOutputStream();
+		PrintWriter pwrite=new PrintWriter(ostream,true);
+		pwrite.println(fname);
+		
+		InputStream istream=sock.getInputStream();
+		BufferedReader socketRead=new BufferedReader(new InputStreamReader(istream));
+		
+		String str;
+		while((str=socketRead.readLine())!=null){
+			System.out.println(str);
+		}
+		
+		sock.close();
+		socketRead.close();
+		pwrite.close();
+	}
 }
 
+
+/*
+Output:
+$ java TCPS
+Server is ready for connection
+Connection is established and server is waiting for client request
+Client request fulfilled
+
+$ java TCPC
+Enter the filename
+sample.txt
+You are genius
+*/
