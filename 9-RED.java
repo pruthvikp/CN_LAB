3a09691fcfe5a7b4a5ebed76f1a074d99dba6c2e
Pruthvi K P <117765336+pruthvikp@users.noreply.github.com>
2024-01-09 16:31:39 +0530

    Update 9-RED.java

--- 9-RED.java
+++ 9-RED.java
@@ -1,4 +1,4 @@
-//
+// Write a program to implement random early detection (RED) congestion control algorithm.
 
 import java.util.*;
 
@@ -15,7 +15,7 @@ public class RED{
 		
 		int queueLen=0;
 		
-		for(int i=0;i<n;i++){
+		for(int i=1;i<=n;i++){
 			if(queueLen>=queueSize)
 				System.out.println("Packet-"+i+" dropped (Queue full)");
 			else if(rand.nextFloat()<dropProb){
@@ -23,53 +23,51 @@ public class RED{
 				dropProb+=(maxProb-minProb)/(n-1);
 			}
 			else{
-				System.out.println("Packet accepted");
+				System.out.println("Packet-"+i+" accepted");
 				queueLen++;
 				dropProb=minProb;
 			}	
 		}
 	}
 }
-				
-		
+
+
 /*
 Output:
-------------------------------
+------------------------------------------
 Enter the queue size: 
 10
 Enter the number of packets: 
 12
-Packet-0 dropped (Random)
-Packet accepted
-Packet accepted
-Packet accepted
-Packet accepted
-Packet accepted
-Packet accepted
-Packet accepted
-Packet accepted
-Packet-9 dropped (Random)
+Packet-1 accepted
+Packet-2 accepted
+Packet-3 accepted
+Packet-4 dropped (Random)
+Packet-5 accepted
+Packet-6 dropped (Random)
+Packet-7 accepted
+Packet-8 dropped (Random)
+Packet-9 accepted
 Packet-10 dropped (Random)
-Packet accepted
--------------------------------
+Packet-11 accepted
+Packet-12 accepted
+-------------------------------------------
 Enter the queue size: 
-10
+5
 Enter the number of packets: 
-15
-Packet accepted
-Packet accepted
-Packet-2 dropped (Random)
-Packet accepted
-Packet accepted
+12
+Packet-1 accepted
+Packet-2 accepted
+Packet-3 accepted
+Packet-4 accepted
 Packet-5 dropped (Random)
-Packet accepted
-Packet-7 dropped (Random)
-Packet accepted
-Packet accepted
-Packet accepted
-Packet accepted
-Packet-12 dropped (Random)
-Packet accepted
-Packet-14 dropped (Queue full)
--------------------------------
+Packet-6 dropped (Random)
+Packet-7 accepted
+Packet-8 dropped (Queue full)
+Packet-9 dropped (Queue full)
+Packet-10 dropped (Queue full)
+Packet-11 dropped (Queue full)
+Packet-12 dropped (Queue full)
+------------------------------------------
 */
+		
