3d89d085cdc19706578de026de1440fd7e991ad5
Pruthvi K P <117765336+pruthvikp@users.noreply.github.com>
2024-01-09 16:21:07 +0530

    Update TCPS.java

--- 4-TCP/TCPS.java
+++ 4-TCP/TCPS.java
@@ -1,36 +1,41 @@
 // Using TCP/IP sockets, write a client – server program to make the client send the file name and to make the server send back the contents of the requested file if present.
 
- import java.net.*;
+import java.util.*;
+import java.net.*;
 import java.io.*;
 
 public class TCPS{
-    public static void main(String[] args) throws Exception{
-
-        ServerSocket sersock=new ServerSocket(4000);
-        System.out.println("Server ready for connection");
-
-        Socket sock=sersock.accept();
-        System.out.println("Connection is successful and waiting for the client request");
-        
-        InputStream istream=sock.getInputStream();
-        BufferedReader fileRead=new BufferedReader(new InputStreamReader(istream));
-        String fname=fileRead.readLine();
-
-        BufferedReader contentRead=new BufferedReader(new FileReader(fname));
-
-        OutputStream ostream=sock.getOutputStream();
-        PrintWriter pwrite=new PrintWriter(ostream,true);
-
-        String str;
-        while((str=contentRead.readLine())!=null){
-            pwrite.println(str);
-        }
-        sock.close();
-        sersock.close();
-        fileRead.close();
-        contentRead.close();
-        pwrite.close();
-    }
+	public static void main(String[] args) throws Exception{
+		
+		ServerSocket sersock=new ServerSocket(4000);
+		System.out.println("Server is ready for connection");
+		
+		Socket sock=sersock.accept();
+		System.out.println("Connection is established and server is waiting for client request");
+		
+		InputStream istream=sock.getInputStream();
+		BufferedReader fileRead=new BufferedReader(new InputStreamReader(istream));
+		String fname=fileRead.readLine();
+		
+		BufferedReader contentRead=new BufferedReader(new FileReader(fname));
+		
+		OutputStream ostream=sock.getOutputStream();
+		PrintWriter pwrite=new PrintWriter(ostream,true);
+		
+		String str;
+		while((str=contentRead.readLine())!=null){
+			pwrite.println(str);
+		}
+		
+		sersock.close();
+		sock.close();
+		fileRead.close();
+		contentRead.close();
+		pwrite.close();
+		
+		System.out.println("Client request fulfilled");
+	}
 }
+
 
 
